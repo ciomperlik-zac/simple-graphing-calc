ca0d59ce277a575691a3348f680e0f4aa0890085
ciomperlik-zac <148258873+ciomperlik-zac@users.noreply.github.com>
2024-02-15 14:55:24 +0000

    idk

--- main.py
+++ main.py
@@ -13,11 +13,17 @@ points = []
 
 equation = input("Enter Enquation: ")
 
-for point in range(-10,11):
+for point in range(-5,6):
     equation_copy = equation
     
     while equation_copy.find("X") != -1:
-        equation_copy[equation_copy("X")] = point
+        point = equation_copy[equation_copy.find("X")]
     
-    points.append([point,eval(equation_copy)])
+    points.append([point-1,round(eval(equation_copy))-1])
+
+for y in range(10):
+    graph[graph[y]][y] = "#"
+
+for row in graph:
+    print("".join(row))
 
