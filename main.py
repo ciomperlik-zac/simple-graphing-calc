5aef2de7c7a09d5f353873b2d23bbe1fe8a9846e
ciomperlik-zac <148258873+ciomperlik-zac@users.noreply.github.com>
2024-02-15 20:50:37 +0000

    idk

--- main.py
+++ main.py
@@ -6,17 +6,15 @@ points = []
 
 equation = input("Enter Enquation: ")
 
-for point in range(20,-21,-1):
+for point in range(-20,21):
     equation_copy = equation.replace("X",str(point))
+    solution = round(eval(equation_copy))
 
-    points.append(round(eval(equation_copy)))
+    if solution < 20 and round(eval(equation_copy)) >= -20:
+        graph[solution][point] = "◼ "
 
 for point in range(len(points)):
     print("("+str(point-10)+","+str(points[point])+")")
-
-for i in range(1,41):
-    if points[i] >= -20 and points[i] < 20:
-        graph[points[i]+20][i-1] = "◼ "
 
 for row in graph:
     print("".join(row))
